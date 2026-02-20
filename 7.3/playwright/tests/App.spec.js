8c586f92fa223565b5b6f5ef281247a7ec257e58
Vlad Kazachenko <vladkazachenko95@gmail.com>
2026-02-20 20:54:28 +0300

    7.3 Задача 2

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -1,22 +1,48 @@
 const { test, expect } = require("@playwright/test");
+const { email, password } = require("../user");
 
-test("test", async ({ page }) => {
-  // Go to https://netology.ru/free/management#/
-  await page.goto("https://netology.ru/free/management#/");
+test("Успешная авторизация", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
 
-  // Click a
-  await page.click("a");
-  await expect(page).toHaveURL("https://netology.ru/");
+  const loginByEmailButton = page.locator('div.styles_button__MYGdj', { hasText: 'Войти по почте' });
+  await loginByEmailButton.waitFor({ state: 'visible', timeout: 15000 });
+  await loginByEmailButton.click();
 
-  // Click text=Учиться бесплатно
-  await page.click("text=Учиться бесплатно");
-  await expect(page).toHaveURL("https://netology.ru/free");
+  const emailInput = page.locator('input[placeholder="Email"]');
+  const passwordInput = page.locator('input[name="password"]');
+  await emailInput.waitFor({ state: 'visible', timeout: 10000 });
+  await passwordInput.waitFor({ state: 'visible', timeout: 10000 });
 
-  page.click("text=Бизнес и управление");
+  await emailInput.fill(email);
+  await passwordInput.fill(password);
 
-  // Click text=Как перенести своё дело в онлайн
-  await page.click("text=Как перенести своё дело в онлайн");
-  await expect(page).toHaveURL(
-    "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
-  );
+  const submitButton = page.locator('button[data-testid="login-submit-btn"]');
+  await submitButton.waitFor({ state: 'visible', timeout: 5000 });
+  await submitButton.click();
+
+  // await expect(page).toHaveURL(/profile/);
+  // await expect(page.locator("h2")).toBeVisible();
+});
+
+test("Неуспешная авторизация", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+
+  const loginByEmailButton = page.locator('div.styles_button__MYGdj', { hasText: 'Войти по почте' });
+  await loginByEmailButton.waitFor({ state: 'visible', timeout: 15000 });
+  await loginByEmailButton.click();
+
+  const emailInput = page.locator('input[placeholder="Email"]');
+  const passwordInput = page.locator('input[name="password"]');
+  await emailInput.waitFor({ state: 'visible', timeout: 10000 });
+  await passwordInput.waitFor({ state: 'visible', timeout: 10000 });
+
+  await emailInput.fill("12345");
+  await passwordInput.focus(); 
+
+  const errorMessage = page.locator('div.Input_error__R_3Gz > span', { hasText: 'Неверный email' });
+  await expect(errorMessage).toBeVisible();
+
+ 
+  // await passwordInput.fill("любое_значение");
+  // await page.locator('button[data-testid="login-submit-btn"]').click();
 });
